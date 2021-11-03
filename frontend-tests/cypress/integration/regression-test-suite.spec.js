a4464089eafbac6689a6e247ff99c8b28e5c790f
huhu2001 <hui.huang@yh.nackademin.se>
2021-11-03 17:04:28 +0100

    Creat a gitgnore

---
+++ frontend-tests/cypress/integration/regression-test-suite.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+//this is test suite
+describe("Regression test suite",function(){
+//this is test case
+it("login user",function(){
+    cy.visit("http://localhost:3000")
+
+})
+
+
+
+
+
+})
